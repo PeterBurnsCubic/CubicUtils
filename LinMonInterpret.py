a27dd9a779a3589cd07cda7a79c8de3d6ce9c425
Peter Burns <Peter.Burns@cubic.com>
2023-10-19 16:15:32 +0100

    LinMonInterpret - fix problems with bad data within the file

--- LinMonInterpret.py
+++ LinMonInterpret.py
@@ -3,19 +3,23 @@
 import sys
 import struct
 import xlrd
+import re
 
-def findFirstPacket(fh):
-    data = fh.read(100)
-    pos = data.find(b'COM')
-    if pos < 0:
-        raise Exception('Could not find COM in first 100 bytes')
-    fh.seek(pos)
+PacketStartRx = re.compile(b'COM[12]:')
+
+def skipBadData(fh, e):
+    pos = fh.tell()
+    data = fh.read(1000)
+    dpos = PacketStartRx.search(data)
+    dlen = dpos.start() if dpos and dpos.start() > 0 else len(data)
+    print("***** bad data at pos {} ({} bytes: {}): {} *****".format(pos, dlen, data[:dlen].hex(), e))
+    fh.seek(pos + dlen)
 
 def readPacket(fh):
     src = fh.read(5).decode("ascii")
     ts = xlrd.xldate_as_datetime(struct.unpack('d', fh.read(8))[0], 0)
-    len = int.from_bytes(fh.read(4), 'little')
-    data = fh.read(len)
+    dlen = int.from_bytes(fh.read(4), 'little')
+    data = fh.read(dlen)
     return (src, ts, data)
 
 def interpretFile(fname):
@@ -24,12 +28,25 @@ def interpretFile(fname):
     print('')
     try:
         with open(fname, "rb") as fh:
-            findFirstPacket(fh)
-            while True:
-                (src, ts, data) = readPacket(fh)
-                print("{} {}: {}".format(src, ts, data.hex()))
-    except Exception:
-        pass
+            while fh.read(1):
+                fh.seek(-1,1)
+                pos = fh.tell()
+                try:
+                    (src, ts, data) = readPacket(fh)
+                    dpos = PacketStartRx.search(data)
+                    if dpos is not None:
+                        olddlen = len(data)
+                        dlen = dpos.start()
+                        print("{} {}: {} ({} bytes truncated to {})".format(src, ts, data[:dlen].hex(), olddlen, dlen))
+                        fh.seek(pos + 17 + dlen)  # 17 = src (5 bytes), ts (8 bytes) and len (4 bytes)
+                        continue
+                except Exception as e:
+                    fh.seek(pos)
+                    skipBadData(fh, e)
+                else:
+                    print("{} {}: {}".format(src, ts, data.hex()))
+    except Exception as e:
+        print(e, file=sys.stderr)
     print('')
 
 if len(sys.argv) < 2:
